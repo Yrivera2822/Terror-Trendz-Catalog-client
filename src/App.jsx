46aaa47ecfb52212d6fbc30cd293f154cd270325
Abner Nieves <nieves.abner@gmail.com>
2024-06-02 18:50:27 -0400

    Customizing Tailwind, added nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 import React from 'react'
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
+// import AudioPlayer from 'react-h5-audio-player';
+// import 'react-h5-audio-player/lib/styles.css';
+import axios from 'axios';
 // import 'react-h5-audio-player/lib/styles.less' Use LESS
 // import 'react-h5-audio-player/src/styles.scss' Use SASS
 
-const Player = () => (
-  <AudioPlayer
-    autoPlay
-    src="http://example.com/audio.mp3"
-    onPlay={e => console.log("onPlay")}
-    // other props here
-  />
-);
+// const Player = () => (
+//   <AudioPlayer
+//     autoPlay
+//     src="http://example.com/audio.mp3"
+//     onPlay={e => console.log("onPlay")}
+//     // other props here
+//   />
+// );
 function App() {
-  import React from 'react'
   
   function App() {
     return (
       <div>
-        
+        <Navbar />
       </div>
     )
-  }
-  
-  export default App
-  
+  }  
 }
 
 export default App
